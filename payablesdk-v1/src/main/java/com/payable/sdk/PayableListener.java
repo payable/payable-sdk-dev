1cdb79962128fb5b1c84a90afcca7117f29a5e97
Aslam Anver <slam.anver9@gmail.com>
2019-09-20 15:30:13 +0530

    IMPL

--- payablesdk-v1/src/main/java/com/payable/sdk/PayableListener.java
+++
@@ -1,7 +0,0 @@
-package com.payable.sdk;
-
-public interface PayableListener {
-    boolean onPaymentStart(PayableSale payableSale);
-    void onPaymentSuccess(PayableSale payableSale);
-    void onPaymentFailure(PayableSale payableSale);
-}
